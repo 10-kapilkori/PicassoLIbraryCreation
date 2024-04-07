23776566c91d16b3352866424c7364641a9ac867
Kapil Kori <Kapil.Kori@pardypanda.com>
2024-04-07 23:00:52 +0530

    build.gradle file for app module reverted back to application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.library")
+    id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("maven-publish")
+//    id("maven-publish")
 }
 
 android {
@@ -9,7 +9,10 @@ android {
     compileSdk = 34
 
     defaultConfig {
+        applicationId = "com.task.picassolibrarycreation"
         minSdk = 24
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -46,16 +49,16 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "com.github.10-kapilkori"
-            artifactId = "GenerateImage"
-            version = "1.0.7"
-
-            afterEvaluate {
-                from(components["release"])
-            }
-        }
-    }
-}
+//publishing {
+//    publications {
+//        register<MavenPublication>("release") {
+//            groupId = "com.github.10-kapilkori"
+//            artifactId = "GenerateImage"
+//            version = "1.0.7"
+//
+//            afterEvaluate {
+//                from(components["release"])
+//            }
+//        }
+//    }
+//}
